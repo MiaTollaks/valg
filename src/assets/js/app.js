4a7e5fd6d180951f27d68105e6c8b7e91ccb8134
miatollaksvik <design@miatollaksvik.no>
2017-09-21 12:45:01 +0200

    swipe start

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -11,19 +11,49 @@ import Foundation from 'foundation-sites';
 
 $(document).foundation();
 
-$(function(){
-    cardNext();
-    
-});
+//$(function(){
+//    cardNext();
+//    
+//});
+//
+//function cardNext() {
+//    $('.btn').on('click', function(){
+//        $(this).addClass('clicked');
+//        $(this).siblings().removeClass('clicked');
+//        
+//        $('.back-next').css('display', 'block');  
+//       
+//         
+//    }); // end click
+//    
+//} // end cardNext()
 
-function cardNext() {
-    $('.btn').on('click', function(){
-        $(this).addClass('clicked');
-        $(this).siblings().removeClass('clicked');
-        
-        $('.back-next').css('display', 'block');  
-       
-         
-    }); // end click
-    
-} // end cardNext()
+
+$(document).ready(function(){
+
+    $(".card-head").on("swiperight",function(){
+      $(this).addClass('rotate-left').delay(700).fadeOut(1);
+      $('.card-head').find('.status').remove();
+
+      $(this).append('<div class="status like">Like!</div>');      
+      if ( $(this).is(':last-child') ) {
+        $('.card-head:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+       } else {
+          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+       }
+    });  
+
+   $(".card-head").on("swipeleft",function(){
+    $(this).addClass('rotate-right').delay(700).fadeOut(1);
+    $('.card-head').find('.status').remove();
+    $(this).append('<div class="status dislike">Dislike!</div>');
+
+    if ( $(this).is(':last-child') ) {
+     $('.card-head:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+      alert('OUPS');
+     } else {
+        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    } 
+  });
+
+});
